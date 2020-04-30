295b53a010c88620712dd2b47705670bbe6fd9b6
Alexander R. Torrijos <artorrijos@gmail.com>
2020-04-30 19:30:45 +0800

    Docs updated

---
+++ docs/build/precache-manifest.d0c8833bec8f1d860eee24760cfa8f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e763e2045ead595ec1c7050aeecc2ba",
+    "url": "/spacex/build/index.html"
+  },
+  {
+    "revision": "102ee0bfcefaf69e8c78",
+    "url": "/spacex/build/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0fb1290376d71704e844",
+    "url": "/spacex/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "102ee0bfcefaf69e8c78",
+    "url": "/spacex/build/static/js/2.83d9c872.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/spacex/build/static/js/2.83d9c872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb1290376d71704e844",
+    "url": "/spacex/build/static/js/main.af23157f.chunk.js"
+  },
+  {
+    "revision": "41f3b15415cdf6faa9e6",
+    "url": "/spacex/build/static/js/runtime-main.128f3439.js"
+  }
+]);
--- docs/build/service-worker.js
+++ docs/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d56f3742ac404b9cde0c1f178223e8eb.js"
+  "/spacex/build/precache-manifest.d0c8833bec8f1d860eee24760cfa8f3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spacex/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

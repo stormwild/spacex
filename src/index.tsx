e6f791a4d1e6867508ca6c10a1aa21d0a6ac9b6e
Alexander R. Torrijos <artorrijos@gmail.com>
2020-04-30 19:48:48 +0800

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,8 @@ import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
   uri: 'https://spacexdata.herokuapp.com/graphql',
@@ -28,4 +27,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
